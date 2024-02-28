6cf2f3da28008a901d21f614420c90b91381ad5e
Krylov Dmitriy <dimakrylov2001@yandex.ru>
2024-02-28 14:50:58 +0300

    change method for saving author field

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -4,14 +4,14 @@ from posts.models import Group, Post, Comment
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.SerializerMethodField()
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
 
     class Meta:
         model = Post
         fields = '__all__'
-
-    def get_author(self, obj):
-        return obj.author.username
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -21,12 +21,12 @@ class GroupSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SerializerMethodField()
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
 
     class Meta:
         model = Comment
         fields = '__all__'
-        read_only_fields = ('post', )
-
-    def get_author(self, obj):
-        return obj.author.username
+        read_only_fields = ('post',)
